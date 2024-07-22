de04bdf42f62d4f96175cc76ed7776cd4dcf4794
shresthacodes <invinciblecodes@gmail.com>
2024-07-22 23:24:12 +0530

    ss

--- calorie/tracker/views.py
+++ calorie/tracker/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render
 
-from .secrets import Nutrition_Key 
+from .secrets import Nutrition_Key
 
-# 
-# Create your views here.
+
 def home(request):
     import json
     import requests
-    if request.method=="POST":
-        query=request.POST['query']
-        api_url='https://api.api-ninjas.com/v1/nutrition?query='
-        api_request= requests.get(api_url+ query,headers={'X-Api-Key': Nutrition_Key})
+    if request.method == "POST":
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get(
+            api_url + query, headers={'X-Api-Key': Nutrition_Key})
         try:
             api = json.loads(api_request.content)
             print(api_request.content)
         except Exception as e:
-            
+
             api = "oops! There was an error"
             print(e)
         return render(request, 'home.html', {'api': api})
     else:
-        return render(request,"home.html",{'query':"Enter an valid query"})
-    
-
+        return render(request, "home.html", {'query': "Enter an valid query"})
